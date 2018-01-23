896bc03cfaa0abbc4708517fe561779a510d7dc3
Krishna Pradeep <psrinivas@mobiware.com>
2018-01-23 21:12:32 +0530

    Working on account settings view controller

--- POSTABLUR/Postablur/AccountSettingsVC/AccountSettingsVC.swift
+++ POSTABLUR/Postablur/AccountSettingsVC/AccountSettingsVC.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class AccountSettingsVC: UIViewController {
 
+    let headersInfoArray : [String] = ["CONNECTION INFORMATION","ACCOUNT INFORMATION","LEGAL INFORMATION"];
+   
+    let connectionInfoArray : [String] = ["FIND CONTACTS","INVITE CONTACTS"]
+                                    
+    let accInfoArray : [String] = ["EDIT ACCOUNT","CHANGE PASSWORD","ACCOUNT NOTIFICATIONS","ACCOUNT STATS"];
+
+    let legalnfoArray : [String] = ["PRIVACY POLICY","TERMS AND CONDITIONS","ACCOUNT NOTIFICATIONS","ACCOUNT STATS","LOGOUT"];
+
+    
     @IBOutlet weak var accountsTableView: UITableView!
 
     override func viewDidLoad() {
@@ -33,7 +42,7 @@ extension AccountSettingsVC : UITableViewDataSource, UITableViewDelegate
     // MARK: Table view data source
     func numberOfSections(in tableView: UITableView) -> Int
     {
-        return 3
+        return headersInfoArray.count
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
@@ -45,13 +54,32 @@ extension AccountSettingsVC : UITableViewDataSource, UITableViewDelegate
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 1
-        
+        switch indexPath.section
+        {
+        case 0:
+            return headersInfoArray.count
+        case 1:
+            return connectionInfoArray.count
+        default:
+            return legalnfoArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell : AccountsSettingsCell = tableView.dequeueReusableCell(withIdentifier: CellIdentifiers.AccountsSettingsCellIdentifier.rawValue, for: indexPath) as! AccountsSettingsCell
+        switch indexPath.section
+        {
+        case 0:
+            cell.textLabel?.text =  connectionInfoArray[indexPath.row]
+            break
+        case 1:
+            cell.textLabel?.text =  accInfoArray[indexPath.row]
+            break
+        default:
+            cell.textLabel?.text =  legalnfoArray[indexPath.row]
+            break
+        }
             return cell
        
     }
