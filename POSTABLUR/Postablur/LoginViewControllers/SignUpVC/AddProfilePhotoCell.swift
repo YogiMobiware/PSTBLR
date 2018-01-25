1c111cbd67c567ad3382757df2f927eaa687810a
Saraschandra <naveen.chvs@gmail.com>
2018-01-25 14:52:14 +0530

    1.Done some minor changes in signupvc.

--- POSTABLUR/Postablur/LoginViewControllers/SignUpVC/AddProfilePhotoCell.swift
+++ POSTABLUR/Postablur/LoginViewControllers/SignUpVC/AddProfilePhotoCell.swift
@@ -67,8 +67,11 @@ class AddProfilePhotoCell: UITableViewCell
             self.capturedImagefromCamera?.layer.cornerRadius = self.capturedImagefromCamera!.frame.size.width / 2
             self.capturedImagefromCamera?.clipsToBounds = true
             self.capturedImagefromCamera?.layer.masksToBounds = true
-           self.capturedImagefromCamera.image = image
+            self.capturedImagefromCamera.image = image
             self.captureImageView.image = nil
+            self.selectedImageFromPhoto.image = nil
+            self.uploadImageView.image = UIImage.init(named: "userGreyUploadPhotoIcon")
+
         }
         else
         {
@@ -77,8 +80,8 @@ class AddProfilePhotoCell: UITableViewCell
             self.selectedImageFromPhoto?.layer.masksToBounds = true
             self.selectedImageFromPhoto.image = image
             self.uploadImageView.image = nil
-
-            
+            self.capturedImagefromCamera.image = nil
+            self.captureImageView.image = UIImage.init(named: "createGreyIconPostablurApp")
         }
     }
 }
