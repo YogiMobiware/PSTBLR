3865f7231894105726c4654d50484122436fd228
Krishna Pradeep <psrinivas@mobiware.com>
2018-01-25 21:02:43 +0530

    committ to pull

--- POSTABLUR/Postablur/CreatePost/PBCaptureMediaVC.swift
+++ POSTABLUR/Postablur/CreatePost/PBCaptureMediaVC.swift
@@ -122,9 +122,7 @@ class PBCaptureMediaVC: UIViewController,AVCapturePhotoCaptureDelegate
         let shareLabelfontSize = ((UIScreen.main.bounds.size.width) / CGFloat(414.0)) * 24
         let roundedBoldfontSize = floor(shareLabelfontSize)
         self.createTitleLabel.font = self.createTitleLabel.font.withSize(roundedBoldfontSize)
-        self.selectPhotoTypeButton.titleLabel?.textColor = Constants.headerTileColor
-        self.selectAudioTypeButton.titleLabel?.textColor = Constants.whiteTextColor
-        self.selectVideoTypeButton.titleLabel?.textColor = Constants.whiteTextColor
+       
 
 
     }
@@ -139,6 +137,10 @@ class PBCaptureMediaVC: UIViewController,AVCapturePhotoCaptureDelegate
         }
         
         self.xButton.layer.cornerRadius = self.xButton.frame.size.width / 2
+        self.selectPhotoTypeButton.titleLabel?.textColor = Constants.headerTileColor
+        self.selectAudioTypeButton.titleLabel?.textColor = Constants.whiteTextColor
+        self.selectVideoTypeButton.titleLabel?.textColor = Constants.whiteTextColor
+
     }
     
     // MARK: Actions
